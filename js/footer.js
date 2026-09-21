11b233531ac48527b9cc7fc2efe2fe0360eaae53
DarkMCode <darkmcode@gmail.com>
2026-09-21 12:01:58 +0200

    Add files via upload

--- js/footer.js
+++ js/footer.js
@@ -1,4 +1,4 @@
-fetch("/TTS/includes/footer.html")
+fetch("/includes/footer.html")
 .then(r => r.text())
 .then(html => {
     document.getElementById("footer").innerHTML = html;
--- js/header.js
+++ js/header.js
@@ -1,4 +1,4 @@
-fetch("/TTS/includes/header.html")
+fetch("/includes/header.html")
     .then(r => r.text())
     .then(html => {
         document.getElementById("header").innerHTML = html;
